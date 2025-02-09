038a59ecede2b6bdff816d7ba19be51362d4a0c1
ub-victor <victoireushindi371@gmail.com>
2025-02-09 20:08:21 +0200

    add code  into the main.js script

--- main.js
+++ main.js
@@ -5,8 +5,16 @@ window.onload = function(){
     async function checkWeather(params) {
         const response = await fetch(apiUrl + `&appid=${apiKey}`)
         var data = await response.json()
+
         console.log(data)
+    
+
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML = data.main.temp;
+    document.querySelector(".humidity").innerHTML = data.main.humidity;
+    document.querySelector(".wind").innerHTML = data.wind.speed;
     }
 
     checkWeather();
+
 }
