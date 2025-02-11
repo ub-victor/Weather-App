f29967cfcb20e9f683da02c87adb3edbe4171719
ub-victor <victoireushindi371@gmail.com>
2025-02-11 21:21:20 +0200

    codition for img

--- main.js
+++ main.js
@@ -1,20 +1,39 @@
 window.onload = function(){
     const apiKey = "da5d77d535345f098b3d4648bf4185e6";
-    const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=kigali";
+    const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
-    async function checkWeather(params) {
-        const response = await fetch(apiUrl + `&appid=${apiKey}`)
+    const searchBox = document.querySelector(".search input");
+    const searchBtn = document.querySelector(".search button");
+    const weatherIcon = document.querySelector(".weather-icon")
+
+    async function checkWeather(city) {
+        const response = await fetch(apiUrl + city + `&appid=${apiKey}`)
         var data = await response.json()
 
-        console.log(data)
+        console.log(data);
     
 
     document.querySelector(".city").innerHTML = data.name;
-    document.querySelector(".temp").innerHTML = data.main.temp;
-    document.querySelector(".humidity").innerHTML = data.main.humidity;
-    document.querySelector(".wind").innerHTML = data.wind.speed;
+    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°c";
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+    document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+
+    if(data.weather[0].main == "Clouds"){
+        weatherIcon.src = "img/clouds.png";
+    }else if(data.weather[0].main == "Clear"){
+        weatherIcon.src = "img/clear.png";
+    }else if(data.weather[0].main == "Rain"){
+        weatherIcon.src = "img/rain.png";
+    }else if(data.weather[0].main == "Drizzle"){
+        weatherIcon.src = "img/drizzle.png";
+    }else if(data.weather[0].main == "Mist"){
+        weatherIcon.src = "img/mist.png";
     }
 
-    checkWeather();
+    }
+
+    searchBtn.addEventListener("click", ()=>{
+        checkWeather(searchBox.value);
+    })
 
 }
