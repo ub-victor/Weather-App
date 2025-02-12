919f45d01877b0672f2530c5bdd03177580202b8
ub-victor <victoireushindi371@gmail.com>
2025-02-12 09:11:41 +0200

    make iteractive codition , ES6 stucture and react on the APP and style resposiveness in the CSS

--- main.js
+++ main.js
@@ -7,29 +7,37 @@ window.onload = function(){
     const weatherIcon = document.querySelector(".weather-icon")
 
     async function checkWeather(city) {
-        const response = await fetch(apiUrl + city + `&appid=${apiKey}`)
-        var data = await response.json()
+        const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
 
-        console.log(data);
-    
 
-    document.querySelector(".city").innerHTML = data.name;
-    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°c";
-    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
-    document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+        if(response.status == 404){
+            document.querySelector(".error").style.display = "block";
+            document.querySelector(".weather").style.display = "none";
+        }else{
+            var data = await response.json()
 
-    if(data.weather[0].main == "Clouds"){
-        weatherIcon.src = "img/clouds.png";
-    }else if(data.weather[0].main == "Clear"){
-        weatherIcon.src = "img/clear.png";
-    }else if(data.weather[0].main == "Rain"){
-        weatherIcon.src = "img/rain.png";
-    }else if(data.weather[0].main == "Drizzle"){
-        weatherIcon.src = "img/drizzle.png";
-    }else if(data.weather[0].main == "Mist"){
-        weatherIcon.src = "img/mist.png";
-    }
-
+            document.querySelector(".city").innerHTML = data.name;
+            document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°c";
+            document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+            document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+        
+            if(data.weather[0].main == "Clouds"){
+                weatherIcon.src = "img/clouds.png";
+            }else if(data.weather[0].main == "Clear"){
+                weatherIcon.src = "img/clear.png";
+            }else if(data.weather[0].main == "Rain"){
+                weatherIcon.src = "img/rain.png";
+            }else if(data.weather[0].main == "Drizzle"){
+                weatherIcon.src = "img/drizzle.png";
+            }else if(data.weather[0].main == "Mist"){
+                weatherIcon.src = "img/mist.png";
+            }
+            document.querySelector(".weather").style.display = "block";
+            document.querySelector(".error").style.display = "none";
+        }
+        
+        
+       
     }
 
     searchBtn.addEventListener("click", ()=>{
